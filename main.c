77a858b0ae985e9d1ad0a15577e3e5ad5e13a627
r2iz <nere531@outlook.com>
2025-07-09 17:23:09 +0900

    TokenとTokenKindをncc.hに移動

--- main.c
+++ main.c
@@ -1,20 +1,5 @@
 #include "ncc.h"
 
-typedef enum {
-  TK_RESERVED,
-  TK_NUM,
-  TK_EOF,
-} TokenKind;
-
-typedef struct Token Token;
-
-struct Token {
-  TokenKind kind;
-  Token *next;
-  int val;
-  char *str;
-};
-
 Token *token;
 
 void error(char *fmt, ...) {
--- ncc.h
+++ ncc.h
@@ -4,3 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+extern Token *token;
+
+void error(char *fmt, ...);
+
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
