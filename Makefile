04c620c57b28561069a581c76fc35f76332e809b
r2iz <nere531@outlook.com>
2025-07-09 18:58:00 +0900

    add parse.c, tokenize.c

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -std=c11 -g -static
-SRCS = main.c lexer.c
+SRCS = main.c parse.c tokenize.c
 OBJS = $(SRCS:.c=.o)
 
 ncc: $(OBJS)
--- ncc.h
+++ ncc.h
@@ -32,3 +32,24 @@ int expect_number();
 bool at_eof();
 Token *new_token(TokenKind kind, Token *cur, char *str);
 Token *tokenize(char *p);
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *expr();
+Node *mul();
+Node *primary();
